12cae2008a8240650fa0fe7cd3514d4d30bc441f
David <sykesdavid8@gmail.com>
2020-07-23 14:34:08 +0000

    "using a for loop"

--- ingredients.js
+++ ingredients.js
@@ -1,11 +1,7 @@
 const ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
 
-let i = 0;
-
-while(i < ingredients.length) {
+for (i = 0; i < ingredients.length; i++) {
  
  console.log(ingredients[i])
-
- i++
 
 }
